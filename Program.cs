23d7662803510aa89d1ddf3537510cb7b88c9254
Stanislav <staskornienko01@gmail.com>
2024-11-22 14:10:00 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //string upperCaseValue;
+            //string lowerCaseValue;
+            //string returnedText = "";
+            //string option;
+            //string reverse = "";
+
+            //Console.Write("Enter option : ");
+
+            //text = Console.ReadLine();
+
+
+
+
+
+            //if ( option == "1")
+            //{
+
+            //    Console.Write("Write value : ");
+            //    reverse = Console.ReadLine();
+
+            //    for (int i = reverse.Length - 1; i >= 0; i--)
+            //    {
+            //        returnedText += reverse[i];
+            //    }
+            //    Console.WriteLine($" {returnedText} "  );
+
+            //}
+            //if (option == "2") { 
+            //    Console.Write("Write value : ");
+            //    upperCaseValue = Console.ReadLine();
+            //    Console.WriteLine($"{upperCaseValue.ToUpper()}");
+
+
+            //} if (option == "3") { 
+            //    Console.Write("Write value : ");
+            //    lowerCaseValue = Console.ReadLine();
+            //    Console.WriteLine($"{lowerCaseValue.ToLower()}");
+
+
+            //}
+
+
+
+
+
+
+
+
+        }
+    }
+}
